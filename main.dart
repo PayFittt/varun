15399b25b43bc41c1a73a37365d034b1a1241dae
PayFittt <varungoyalindia@gmail.com>
2025-06-12 10:41:18 -0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+
+void main() {
+  runApp(PayFitApp());
+}
+
+class PayFitApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PayFit',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Roboto',
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
 
